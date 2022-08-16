e09bb1d2c3b19dfed00d5c3c5bc0a0a1c2c63ac5
Monine Chan <mochan@students.42wolfsburg.de>
2022-08-16 23:43:35 +0200

    parsing envp

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:08:12 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/15 18:19:12 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/16 23:42:36 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    char *options[3] = {"ls", "-la", NULL};
+    // char *options[3] = {"ls", "-la", NULL};
 
     (void)argc;
     (void)argv;
-    execve("/bin/ls", options, envp);
+    // execve("/bin/ls", options, envp);
+	int i;
+	i = 1;
+	while (*envp)
+	{
+		ft_printf("envp %d	:	%s\n", i, *envp);
+		envp++;
+		i++;
+	}
     return (0);
 }
